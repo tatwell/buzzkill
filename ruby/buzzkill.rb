65400747649a19a959861aab6b742b0ef638fb35
Tom Atwell <klenwell@gmail.com>
2021-06-26 14:27:42 -0700

    Stub out buzzkill script.

---
+++ ruby/buzzkill.rb
@@ -0,0 +1,24 @@
+#
+# Solves daily NY Times Spelling Bee puzzle:
+# https://www.nytimes.com/puzzles/spelling-bee
+#
+# Usage:
+# ruby buzzkill.rb "RWYKACT"
+#
+
+#
+# Helper Methods
+#
+
+#
+# Public Interface
+#
+def test; end
+
+def solve; end
+
+#
+# Main
+#
+test
+solve
